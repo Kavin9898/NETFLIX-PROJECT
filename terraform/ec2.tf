6661c7c9518fcf68a0985e43f88c19ebaa40873c
Kavin9898 <kavindharan40828@gmail.com>
2026-03-02 20:51:06 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -17,3 +17,28 @@ resource "aws_launch_template" "web_template" {
     }
   }
 }
+resource "aws_security_group" "NET_SG" {
+  name   = "web-sg"
+  vpc_id = aws_vpc.NET_VPC.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
