c5febe8f710e88d9660b7d8db70c356229234370
Kavin9898 <kavindharan40828@gmail.com>
2026-03-02 23:16:34 +0530

    Update autoscalling.tf

--- terraform/autoscalling.tf
+++ terraform/autoscalling.tf
@@ -1,7 +1,10 @@
 resource "aws_autoscaling_group" "web_asg" {
+  name                 = "web-auto-scaling-group"   # ✅ FIXED NAME
+
   desired_capacity     = 2
   max_size             = 4
   min_size             = 1
+
   vpc_zone_identifier  = [
     aws_subnet.PUBLIC_NET_SUBNET.id,
     aws_subnet.PUBLIC_NET_SUBNET1.id
@@ -13,6 +16,9 @@ resource "aws_autoscaling_group" "web_asg" {
   }
 
   target_group_arns = [aws_lb_target_group.web_tg.arn]
+
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
 
   tag {
     key                 = "Name"
