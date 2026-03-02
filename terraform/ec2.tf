81e8851259296686ce8f144d2ef9f1064fb9ebed
Kavin9898 <kavindharan40828@gmail.com>
2026-03-02 20:53:58 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -42,3 +42,12 @@ resource "aws_security_group" "NET_SG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+#!/bin/bash
+yum update -y
+yum install -y docker
+systemctl start docker
+systemctl enable docker
+
+docker pull 801195563235.dkr.ecr.ap-south-1.amazonaws.com/netflix-devops-app:latest
+
+docker run -d -p 80:3000 801195563235.dkr.ecr.ap-south-1.amazonaws.com/netflix-devops-app:latest
