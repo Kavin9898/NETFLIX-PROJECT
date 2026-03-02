45a7e4cf92eb2b20650f8f978e39712e2e86a044
Kavin9898 <kavindharan40828@gmail.com>
2026-03-02 20:36:43 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -28,17 +28,3 @@ resource "aws_lbautoscaling.tf" "web_alb" {
   ]
   security_groups = [aws_security_group.NET_SG.id]
 }
-
-# ---------------- Listener ----------------
-resource "aws_lb_listener" "web_listener" {
-  load_balancer_arn = aws_lb.web_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.web_tg.arn
-  }
-
-}
-
