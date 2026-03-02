478a4245c4e03013037fc0da16dda3cae2a522d5
Kavin9898 <kavindharan40828@gmail.com>
2026-03-02 20:41:04 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,8 +1,17 @@
-variable "region" {}
+variable "region" {
+  default = "ap-south-1"
+}
+
 variable "ami_id" {}
+
 variable "instance_type" {}
+
 variable "key_name" {}
-variable "db_username" {}
+
+variable "db_username" {
+  default = "admin"
+}
+
 variable "db_password" {
-  sensitive = true
+  default = "Admin@123"
 }
